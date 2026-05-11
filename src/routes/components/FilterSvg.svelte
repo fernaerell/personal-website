91bf8b0757a2c499bfc35ccf52e9b20ee4f812e5
fernaerell <fernaerell2020@gmail.com>
2026-05-12 06:50:26 +0700

    chore: Add Filter Svg

---
+++ src/routes/components/FilterSvg.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	const props = $props();
+	const strokeWidth = 1.5;
+</script>
+
+<svg {...props} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none">
+	<path d="M10 8L20 8" stroke="currentColor" stroke-width={strokeWidth} stroke-linecap="round" />
+	<path d="M4 16L14 16" stroke="currentColor" stroke-width={strokeWidth} stroke-linecap="round" />
+	<ellipse cx="7" cy="8" rx="3" ry="3" stroke="currentColor" stroke-width={strokeWidth} fill="none" />
+	<ellipse cx="17" cy="16" rx="3" ry="3" stroke="currentColor" stroke-width={strokeWidth} fill="none" />
+</svg>
