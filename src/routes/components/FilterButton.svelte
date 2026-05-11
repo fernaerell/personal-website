49b239cf49dcce701fa14060ea73a686a842bdfc
fernaerell <fernaerell2020@gmail.com>
2026-05-12 06:50:46 +0700

    feat: Add filter button component (ui only)

---
+++ src/routes/components/FilterButton.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import FilterSvg from './FilterSvg.svelte';
+</script>
+
+<div class="flex flex-row items-center justify-center text-[white] gap-1.75 px-1.75 py-0.75 border border-[rgba(255,255,255,0.50)] hover:bg-[rgba(255,255,255,0.1)] rounded-[7px] w-fit h-fit hover:cursor-pointer">
+	<FilterSvg class="text-[white] w-5 h-5" />
+	<span>Filter</span>
+</div>
