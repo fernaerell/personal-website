6cbc2ec2f434aed1cf7b291d27ea7368b70c8549
fernaerell <fernaerell2020@gmail.com>
2026-05-11 15:50:06 +0700

    feat: Add search component (ui only)

---
+++ src/routes/components/Search.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import SearchSvg from './SearchSvg.svelte';
+</script>
+
+<div class="flex flex-row items-center gap-2.5 p-2.5">
+	<SearchSvg class="text-[white] w-3.75 h-3.75 hover:cursor-pointer"/>
+	<input class="text-[white] focus:outline-none" type="text" name="search" id="search" placeholder="Search..." />
+</div>
