84ea03b53f62ee6a7ae1b405c012cc90e3373c3c
fernaerell <fernaerell2020@gmail.com>
2026-05-14 17:49:06 +0700

    refactor: Add container for select element and move list of options to ts variable in filter component

--- src/routes/components/Filter.svelte
+++ src/routes/components/Filter.svelte
@@ -1,7 +1,11 @@
-<select name="tag" id="tag">
-    <option value="All" selected>All</option>
-    <option value="Minecraft">Minecraft</option>
-    <option value="GTA V">GTA V</option>
-    <option value="Addon">Addon</option>
-    <option value="Mod">Mod</option>
-</select>
+<script lang="ts">
+	const options = ['All', 'Minecraft', 'GTA V', 'Addon', 'Mod'];
+</script>
+
+<div class="rounded-2xl bg-[#1a1a1a] p-2.5">
+	<select class="pr-1 focus:outline-none" name="tag" id="tag">
+		{#each options as option, index (index)}
+			<option value={option} selected={index == 0}>{option}</option>
+		{/each}
+	</select>
+</div>
