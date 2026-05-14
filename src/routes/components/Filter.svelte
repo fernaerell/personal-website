89e0f6ed9b8e03f229438b70b30479ba08cf7356
fernaerell <fernaerell2020@gmail.com>
2026-05-14 17:13:18 +0700

    refactor: Simplify filter component (ui only)

---
+++ src/routes/components/Filter.svelte
@@ -0,0 +1,7 @@
+<select name="tag" id="tag">
+    <option value="All" selected>All</option>
+    <option value="Minecraft">Minecraft</option>
+    <option value="GTA V">GTA V</option>
+    <option value="Addon">Addon</option>
+    <option value="Mod">Mod</option>
+</select>
